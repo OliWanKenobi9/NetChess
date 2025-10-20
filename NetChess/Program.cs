5b2b8ff2f0b4cf9be068a0701543a2b21dc57347
OliWanKenobi9 <oliver.hitz.7@gmail.com>
2025-10-20 13:22:14 +0200

    initialise pieces, board

--- NetChess/Program.cs
+++ NetChess/Program.cs
@@ -6,45 +6,65 @@ using System.Threading.Tasks;
 
 namespace NetChess
 {
-    public enum pieceType { Pawn, Bishop, Knight, Rook, Queen, King }
+    public enum pieceType { Pawn, Bishop, Knight, Rook, Queen, King, Empty }
     public struct piece
     {
         public pieceType type;
         public bool isWhite;
-        (int, int) position;
+        public (int, int) position;
     }
     public struct boardPosition
     {
         public (int, int) position;
+        public piece current;
     }
     internal class Program
     {
-        static void Main(string[] args)
+        static boardPosition[] initialiseBoard()
         {
             boardPosition[] board = new boardPosition[64];
-            piece[] pieces = new piece[32];
-            // Declaration
 
-
-            #region Initialise Variables
-
-            for(int i = 0; i < 64; i++)
+            for (int i = 0; i < 64; i++)
             {
-                for(int j = 0; j < 8; i++)
+                for (int j = 0; j < 8; i++)
                 {
-                    for(int h = 0; h < 8; i++)
+                    for (int h = 0; h < 8; i++)
                     {
                         board[i].position = (j, h);
+                        board[i].current.position = board[i].position;
+                        board[i].current.type = pieceType.Empty;
                     }
                 }
             }
+            return board;
+        }
+        static piece[] initialisePieces()
+        {
+            piece[] pieces = new piece[32];
 
-            for(int i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++)
             {
-
+                pieces[i].type = pieceType.Pawn;
+                pieces[i].isWhite = true;
+                pieces[i].position = (1, i);
+            }
+            for (int i = 8; i < 16; i++)
+            {
+                pieces[i].type = pieceType.Pawn;
+                pieces[i].isWhite = false;
+                pieces[i].position = (1, i);
             }
 
-            #endregion
+            return pieces;
+        }
+        static void Main(string[] args)
+        {
+            boardPosition[] board = initialiseBoard();
+            piece[] pieces = initialisePieces();
+            // Declaration
+
+
+
         }
     }
 }
