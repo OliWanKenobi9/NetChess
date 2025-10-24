7f98a1f21e8123b042e0aaac4743103c7330949c
OliWanKenobi9 <oliver.hitz.7@gmail.com>
2025-10-24 09:01:57 +0200

    function notationFraction

--- NetChess/Program.cs
+++ NetChess/Program.cs
@@ -192,19 +192,114 @@ namespace NetChess
             }
             return board;
         }
-        static piece[] move(piece[] pieces, boardPosition[] board, bool whiteTurn)
+        static piece[] notationMove(piece[] pieces, boardPosition[] board, bool whiteTurn)
         /* PGN Notation: [x1, y1] [x2, y2]
          * e2 e4
          * a5 a6
          * c3 g3
          */
         {
-            string input, in1, in2;
+            string input;
+            (int, int) notationFracStart, notationFracMove;
+
             Console.Write("Notation: ");
-            string input = Console.ReadLine();
+            input = Console.ReadLine();
+
+            ((int x, int y) start, (int x, int y) move) inputFractured = notationFraction(input);
+            if (inputFractured.start.x == -1 || inputFractured.start.y == -1 || inputFractured.move.x == -1 || inputFractured.move.x == -1)
+            {
+                Console.WriteLine("Invalid Input.");
+                return null;
+            }
+            notationFracStart = inputFractured.start;
+            notationFracMove = inputFractured.move;
+
 
 
             return pieces;
+        }
+        static ((int, int) start, (int, int) move) notationFraction(string input)
+        /* Split input into 2 Tulips: ((<notationFrac1>), (<notationFrac2>))
+         * Use Item1/Item2 for notationFrac1/notationFrac2
+         */
+        {
+            ((int x, int y) start, (int x, int y) move) response;
+
+            response = ((-1, -1), (-1, -1));
+
+            switch (input[0])
+            {
+                case 'a':
+                    response.start.x = 1;
+                    break;
+                case 'b':
+                    response.start.x = 2;
+                    break;
+                case 'c':
+                    response.start.x = 3;
+                    break;
+                case 'd':
+                    response.start.x = 4;
+                    break;
+                case 'e':
+                    response.start.x = 5;
+                    break;
+                case 'f':
+                    response.start.x = 6;
+                    break;
+                case 'g':
+                    response.start.x = 7;
+                    break;
+                case 'h':
+                    response.start.x = 8;
+                    break;
+                default:
+                    response.move.x = -1;
+                    break;
+            }
+
+            response.start.y = Convert.ToInt32(input[1]);
+
+            if (response.start.y < 1 || response.start.y > 8)
+                response.start.y = -1;
+
+            switch (input[3])
+            {
+                case 'a':
+                    response.move.x = 1;
+                    break;
+                case 'b':
+                    response.move.x = 2;
+                    break;
+                case 'c':
+                    response.move.x = 3;
+                    break;
+                case 'd':
+                    response.move.x = 4;
+                    break;
+                case 'e':
+                    response.move.x = 5;
+                    break;
+                case 'f':
+                    response.move.x = 6;
+                    break;
+                case 'g':
+                    response.move.x = 7;
+                    break;
+                case 'h':
+                    response.move.x = 8;
+                    break;
+                default:
+                    response.move.x = -1;
+                    break;
+            }
+
+            response.move.y = Convert.ToInt32(input[4]);
+
+            if (response.move.y < 1 || response.move.y > 8)
+                response.move.y = -1;
+
+            return response;
         }
 
         static void Main(string[] args)
@@ -215,18 +310,15 @@ namespace NetChess
             bool whiteTurn;
             // Declaration
 
-            while(true)
-            {
-                if (turn % 2 == 0)
-                    whiteTurn = false;
-                else whiteTurn = true;
+            if (turn % 2 == 0)
+                whiteTurn = false;
+            else whiteTurn = true;
 
-                // Ablauf
-                Console.Clear();
-                board = getPieces(pieces, board);
-                displayBoard(board);
-                pieces = move(pieces, board, whiteTurn);
-            }
+            // Ablauf
+            Console.Clear();
+            board = getPieces(pieces, board);
+            displayBoard(board);
+            pieces = notationMove(pieces, board, whiteTurn);
         }
     }
 }
