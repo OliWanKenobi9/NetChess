4a7bc5badd0ef325c01199496a79b1eddd750669
Oliver Hitz <oliver.hitz.7@gmail.com>
2025-10-25 21:03:18 +0200

    case, replace 2 ifs with ?:

--- NetChess/Program.cs
+++ NetChess/Program.cs
@@ -2,23 +2,23 @@
 
 namespace NetChess
 {
-    public enum pieceType { Pawn, Bishop, Knight, Rook, Queen, King, Empty }
-    public struct piece
+    public enum PieceType { Pawn, Bishop, Knight, Rook, Queen, King, Empty }
+    public struct Piece
     {
-        public pieceType type;
+        public PieceType type;
         public bool isWhite;
         public (int, int) position;
     }
-    public struct boardPosition
+    public struct BoardPosition
     {
         public (int, int) position;
-        public piece current;
+        public Piece current;
     }
     internal class Program
     {
-        static boardPosition[] initialiseBoard()
+        static BoardPosition[] InitialiseBoard()
         {
-            boardPosition[] board = new boardPosition[64];
+            BoardPosition[] board = new BoardPosition[64];
             int index = 0;
 
             for (int j = 0; j < 8; j++)
@@ -27,101 +27,101 @@ namespace NetChess
                 {
                     board[index].position = (j + 1, h + 1);
                     board[index].current.position = board[index].position;
-                    board[index].current.type = pieceType.Empty;
+                    board[index].current.type = PieceType.Empty;
                     index++;
                 }
             }
             return board;
         }
-        static piece[] initialisePieces()
+        static Piece[] InitialisePieces()
         {
-            piece[] pieces = new piece[32];
+            Piece[] pieces = new Piece[32];
 
             for (int i = 0; i < 8; i++)
             {
-                pieces[i].type = pieceType.Pawn;
+                pieces[i].type = PieceType.Pawn;
                 pieces[i].isWhite = true;
                 pieces[i].position = (2, i+1);
             }
             for (int i = 8; i < 16; i++)
             {
-                pieces[i].type = pieceType.Pawn;
+                pieces[i].type = PieceType.Pawn;
                 pieces[i].isWhite = false;
                 pieces[i].position = (7, i - 7);
             }
 
 
-            pieces[16].type = pieceType.Rook;
+            pieces[16].type = PieceType.Rook;
             pieces[16].position = (1, 1);
             pieces[16].isWhite = true;
 
-            pieces[17].type = pieceType.Rook;
+            pieces[17].type = PieceType.Rook;
             pieces[17].position = (1, 8);
             pieces[17].isWhite = true;
 
-            pieces[18].type = pieceType.Rook;
+            pieces[18].type = PieceType.Rook;
             pieces[18].position = (8, 1);
             pieces[18].isWhite = false;
 
-            pieces[19].type = pieceType.Rook;
+            pieces[19].type = PieceType.Rook;
             pieces[19].position = (8, 8);
             pieces[19].isWhite = false;
 
 
-            pieces[20].type = pieceType.Knight;
+            pieces[20].type = PieceType.Knight;
             pieces[20].position = (1, 2);
             pieces[20].isWhite = true;
 
-            pieces[31].type = pieceType.Knight;
+            pieces[31].type = PieceType.Knight;
             pieces[31].position = (1, 7);
             pieces[31].isWhite = true;
 
-            pieces[21].type = pieceType.Knight;
+            pieces[21].type = PieceType.Knight;
             pieces[21].position = (8, 2);
             pieces[21].isWhite = false;
 
-            pieces[22].type = pieceType.Knight;
+            pieces[22].type = PieceType.Knight;
             pieces[22].position = (8, 7);
             pieces[22].isWhite = false;
 
 
-            pieces[23].type = pieceType.Bishop;
+            pieces[23].type = PieceType.Bishop;
             pieces[23].position = (1, 3);
             pieces[23].isWhite = true;
 
-            pieces[24].type = pieceType.Bishop;
+            pieces[24].type = PieceType.Bishop;
             pieces[24].position = (1, 6);
             pieces[24].isWhite = true;
 
-            pieces[25].type = pieceType.Bishop;
+            pieces[25].type = PieceType.Bishop;
             pieces[25].position = (8, 3);
             pieces[25].isWhite = false;
 
-            pieces[26].type = pieceType.Bishop;
+            pieces[26].type = PieceType.Bishop;
             pieces[26].position = (8, 6);
             pieces[26].isWhite = false;
 
 
-            pieces[27].type = pieceType.King;
+            pieces[27].type = PieceType.King;
             pieces[27].position = (1, 5);
             pieces[27].isWhite = true;
 
-            pieces[28].type = pieceType.King;
+            pieces[28].type = PieceType.King;
             pieces[28].position = (8, 4);
             pieces[28].isWhite = false;
 
 
-            pieces[29].type = pieceType.Queen;
+            pieces[29].type = PieceType.Queen;
             pieces[29].position = (1, 4);
             pieces[29].isWhite = true;
 
-            pieces[30].type = pieceType.Queen;
+            pieces[30].type = PieceType.Queen;
             pieces[30].position = (8, 5);
             pieces[30].isWhite = false;
 
             return pieces;
         }
-        static void displayBoard(boardPosition[] board)
+        static void DisplayBoard(BoardPosition[] board)
         {
             for(int i = 0; i < board.Length; i++)
             {
@@ -129,25 +129,25 @@ namespace NetChess
                 
                 switch (board[i].current.type)
                 {
-                    case pieceType.Pawn:
+                    case PieceType.Pawn:
                         symbol = " P ";
                         break;
-                    case pieceType.Bishop:
+                    case PieceType.Bishop:
                         symbol = " B ";
                         break;
-                    case pieceType.Knight:
+                    case PieceType.Knight:
                         symbol = " N ";
                         break;
-                    case pieceType.Rook:
+                    case PieceType.Rook:
                         symbol = " R ";
                         break;
-                    case pieceType.Queen:
+                    case PieceType.Queen:
                         symbol = " Q ";
                         break;
-                    case pieceType.King:
+                    case PieceType.King:
                         symbol = " K ";
                         break;
-                    case pieceType.Empty:
+                    case PieceType.Empty:
                         symbol = "   ";
                         break;
                 }
@@ -156,13 +156,11 @@ namespace NetChess
                 {
                     Console.WriteLine();
                 }
+                
+                
+                Console.BackgroundColor = ((board[i].position.Item1 + board[i].position.Item2) % 2 == 0) ? ConsoleColor.Green: ConsoleColor.DarkYellow;
 
-                if ((board[i].position.Item1 + board[i].position.Item2) % 2 == 0)
-                    Console.BackgroundColor = ConsoleColor.Green;
-                else Console.BackgroundColor = ConsoleColor.DarkYellow;
-
-                if (board[i].current.isWhite)
-                    Console.ForegroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
                 Console.Write(symbol);
 
                 Console.ForegroundColor = ConsoleColor.White;
@@ -173,7 +171,7 @@ namespace NetChess
             Console.WriteLine();
             Console.WriteLine("Pawn: P | Bishop: B | Knight: N | Rook: R | Queen: Q | King: K");
         }
-        static boardPosition[] getPieces(piece[] pieces, boardPosition[] board)
+        static BoardPosition[] GetPieces(Piece[] pieces, BoardPosition[] board)
         {
             for(int i = 0; i < board.Length; i++)
             {
@@ -187,7 +185,7 @@ namespace NetChess
             }
             return board;
         }
-        static piece[] notationMove(piece[] pieces, boardPosition[] board, bool whiteTurn)
+        static Piece[] NotationMove(Piece[] pieces, BoardPosition[] board, bool whiteTurn)
         /* PGN Notation: [x1, y1] [x2, y2]
          * e2 e4
          * a5 a6
@@ -200,7 +198,7 @@ namespace NetChess
             Console.Write("Notation: ");
             input = Console.ReadLine();
 
-            ((int x, int y) start, (int x, int y) move) inputFractured = notationFraction(input);
+            ((int x, int y) start, (int x, int y) move) inputFractured = NotationFraction(input);
             if (inputFractured.start.x == -1 || inputFractured.start.y == -1 || inputFractured.move.x == -1 || inputFractured.move.x == -1)
             {
                 Console.WriteLine("Invalid Input.");
@@ -209,11 +207,11 @@ namespace NetChess
             notationFracStart = inputFractured.start;
             notationFracMove = inputFractured.move;
 
-
+            
 
             return pieces;
         }
-        static ((int, int) start, (int, int) move) notationFraction(string input)
+        static ((int, int) start, (int, int) move) NotationFraction(string input)
         /* Split input into 2 Tulips: ((<notationFrac1>), (<notationFrac2>))
          * Use Item1/Item2 for notationFrac1/notationFrac2
          */
@@ -299,21 +297,22 @@ namespace NetChess
 
         static void Main(string[] args)
         {
-            boardPosition[] board = initialiseBoard();
-            piece[] pieces = initialisePieces();
+            BoardPosition[] board = InitialiseBoard();
+            Piece[] pieces = InitialisePieces();
             int turn = 1;
             bool whiteTurn;
             // Declaration
 
-            if (turn % 2 == 0)
-                whiteTurn = false;
-            else whiteTurn = true;
+            do
+            {
+                whiteTurn = turn % 2 != 0 ? true : false;
 
-            // Ablauf
-            Console.Clear();
-            board = getPieces(pieces, board);
-            displayBoard(board);
-            pieces = notationMove(pieces, board, whiteTurn);
+                Console.Clear();
+                board = GetPieces(pieces, board);
+                DisplayBoard(board);
+                pieces = NotationMove(pieces, board, whiteTurn);
+                turn++;
+            } while (true); // Win condition
         }
     }
 }
